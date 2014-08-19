58d1d61a81fb6034c4374bce15bc147618851ece
bk0606 <albert.bikeev@gmail.com>
2014-08-20 02:32:31 +0400

    Add car with wheels, make camera follow car and clean up project folders

---
+++ Assets/Custom Assets/Scripts/MotorBehaviourScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotorBehaviourScript : MonoBehaviour {
+	public WheelJoint2D DrivingWheel;	
+	public float acceleration = 100;
+	public BoxCollider2D GasPedal;
+	public BoxCollider2D BrakePedal;
+	public Rigidbody2D Car = new Rigidbody2D();
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckKeyboardInput ();
+		CheckMouseInput ();
+	}
+
+	private void CheckKeyboardInput() {
+		float inputVect = Input.GetAxis ("Horizontal");
+		JointMotor2D motor = DrivingWheel.motor;
+		bool isUseMotor = true;
+		if (inputVect == 1) {
+			// TODO: Refactor
+			motor.motorSpeed = motor.maxMotorTorque - motor.motorSpeed > 3 ? 
+				motor.motorSpeed + Mathf.Sqrt(motor.motorSpeed + acceleration)
+				: motor.motorSpeed;
+		} else if (inputVect == -1) {
+			motor.motorSpeed = 0;
+		} else {
+			if (motor.motorSpeed > acceleration) {
+				motor.motorSpeed -= Mathf.Pow(acceleration, 2);
+			} else {
+				motor.motorSpeed = 0;
+				isUseMotor = false;
+			}
+		}
+		DrivingWheel.motor = motor;
+		DrivingWheel.useMotor = isUseMotor;
+	}
+
+	private void CheckMouseInput() {
+		// TODO: write it
+		float mouseX = Input.mousePosition.x;
+		float mouseY = Input.mousePosition.y;
+	}
+}
---
+++ Assets/Custom Assets/Scripts/PlayerFollowBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFollowBehaviour : MonoBehaviour {
+	public Transform Target;
+	
+	// Update is called once per frame
+	void Update () {
+		var selPos = transform.position;
+		selPos.x = Target.position.x;
+		selPos.y = Target.position.y;
+		transform.position = selPos;
+	}
+}
